40bb46eca916c8a3142abe2e04b1ae131d8e782b
kaye garcia <kayemgarcia@gmail.com>
2022-01-21 12:19:14 -0600

    Import BrowserRouter in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './styles/App.css';
+import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Signup from './components/Authentication/Signup';
+import Login from './components/Authentication/Login';
 
 const App = () => {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login/>} />
+      </Routes>
+    </Router>
   );
 }
 
