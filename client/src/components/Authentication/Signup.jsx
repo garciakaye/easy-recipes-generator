41fc837b45ae1da034831991047e55cdf01507a8
kaye garcia <kayemgarcia@gmail.com>
2022-01-21 14:32:48 -0600

    Create signup form

--- client/src/components/Authentication/Signup.jsx
+++ client/src/components/Authentication/Signup.jsx
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { baseUrl, headers } from "../../Globals";
 
 const Signup = () => {
+  const initialFormValues = {
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+  };
+  
+  const [values, setValues] = useState(initialFormValues)
+
+  const handleInputChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const strongParams = {
+      user: {
+        first_name: initialFormValues.firstname,
+        last_name: initialFormValues.lastname,
+        username: initialFormValues.username,
+        password: initialFormValues.password
+      }
+    }
+
+    fetch(baseUrl + "/users", {
+      method: "POST",
+      headers,
+      body: JSON.stringify(strongParams)
+    })
+  }
+
   return (
-    <div>
-      <form>
+    <div className="signup-form">
+      <form
+        id="signup"
+        onSubmit={handleSubmit}
+      >
+        <h1>Create An Account</h1>
         <div>
           <label htmlFor="firstname"></label>
             <input
               type="text"
               name="firstname"
               placeholder="First Name"
+              value={values.firstname}
               id="firstname"
+              onChange={handleInputChange}
             />
         </div>
         <div>
@@ -19,7 +61,9 @@ const Signup = () => {
               type="text"
               name="lastname"
               placeholder="Last Name"
+              value={values.lastname}
               id="lastname"
+              onChange={handleInputChange}
             />
         </div>
         <div>
@@ -28,7 +72,9 @@ const Signup = () => {
               type="text"
               name="username"
               placeholder="Username (must be unique)"
+              value={values.username}
               id="username"
+              onChange={handleInputChange}
             />
         </div>
         <div>
@@ -37,9 +83,17 @@ const Signup = () => {
               type="password"
               name="password"
               placeholder="Password"
-              id="password"
+              value={values.password}
+            id="password"
+            onChange={handleInputChange}
             />
         </div>
+        <button
+          className="signup-btn"
+          id="signUp"
+        >
+          Create User
+        </button>
       </form>
     </div>
   );
