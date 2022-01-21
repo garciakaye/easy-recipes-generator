abe58e22e64eeb95cf68838e0f478cc2f84b36ee
kaye garcia <kayemgarcia@gmail.com>
2022-01-21 16:43:16 -0600

    Add useEffect function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import './styles/App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
@@ -7,12 +7,29 @@ import Signup from './components/Authentication/Signup';
 import Login from './components/Authentication/Login';
 
 const App = () => {
+  const [currentUser, setCurrentUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const loginUser = user => {
+    setCurrentUser(user);
+    setLoggedIn(user);
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('jwt')
+    if (token && !loggedIn) {
+      
+      setLoggedIn(true);
+    }
+  }, [loggedIn])
+
   return (
     <Router>
+      { loggedIn ? <h1>we're loggedin</h1> : null }
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="signup" element={<Signup />} />
+        <Route path="signup" element={<Signup loginUser={ loginUser }/>} />
         <Route path="login" element={<Login/>} />
       </Routes>
     </Router>
