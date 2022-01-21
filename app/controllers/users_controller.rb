1f1c3195e8aee3e38441e50071d8180753f2ff22
kaye garcia <kayemgarcia@gmail.com>
2022-01-21 16:43:36 -0600

    Add encode and decode token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
   before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
@@ -18,9 +19,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: @token }, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: @user.errors.full_messages }, status: :not_acceptable
     end
   end
 
@@ -46,6 +48,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :username, :password_digest)
+      params.require(:user).permit(:first_name, :last_name, :username, :password)
     end
 end
