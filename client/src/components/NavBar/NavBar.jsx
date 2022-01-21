368d673f2766b4593deba93d2403b935b86b9d8d
kaye garcia <kayemgarcia@gmail.com>
2022-01-21 12:20:33 -0600

    Add links to Navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <div>
-      NavBar
+      <ul>
+      <li><Link to="/">Homepage</Link></li>
+      <li><Link to="signup">Signup</Link></li>
+      <li><Link to="login">Login</Link></li>
+      </ul>
     </div>
   );
 };
