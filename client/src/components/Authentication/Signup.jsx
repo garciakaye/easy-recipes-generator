00c81937a61c8ca0f8103b57badaafb9ca5081d3
kaye garcia <kayemgarcia@gmail.com>
2022-01-21 12:19:57 -0600

    Add signup form fields

--- client/src/components/Authentication/Signup.jsx
+++ client/src/components/Authentication/Signup.jsx
@@ -3,7 +3,44 @@ import React from 'react';
 const Signup = () => {
   return (
     <div>
-      Signup
+      <form>
+        <div>
+          <label htmlFor="firstname"></label>
+            <input
+              type="text"
+              name="firstname"
+              placeholder="First Name"
+              id="firstname"
+            />
+        </div>
+        <div>
+          <label htmlFor="lastname"></label>
+            <input
+              type="text"
+              name="lastname"
+              placeholder="Last Name"
+              id="lastname"
+            />
+        </div>
+        <div>
+          <label htmlFor="username"></label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username (must be unique)"
+              id="username"
+            />
+        </div>
+        <div>
+          <label htmlFor="password"></label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              id="password"
+            />
+        </div>
+      </form>
     </div>
   );
 };
