c680e9f50ecc3b9361461b1bca7d09cd83269176
kaye garcia <kayemgarcia@gmail.com>
2022-01-24 09:17:51 -0600

    Create GET fetch request function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import './styles/App.css';
 import React, { useEffect, useState } from 'react';
-import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/Navigation/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Signup from './components/Authentication/Signup';
 import Login from './components/Authentication/Login';
+import { baseUrl, headers, getToken } from "./Globals";
 
 const App = () => {
   const [currentUser, setCurrentUser] = useState({});
@@ -15,18 +16,32 @@ const App = () => {
     setLoggedIn(user);
   }
 
+  function logoutUser(){
+    setCurrentUser({});
+    setLoggedIn(false);
+    localStorage.removeItem('jwt');
+  }
+
   useEffect(() => {
     const token = localStorage.getItem('jwt')
     if (token && !loggedIn) {
-      
-      setLoggedIn(true);
-    }
-  }, [loggedIn])
+      fetch(baseUrl + '/get-current-user', {
+        method: "GET",
+        headers: {
+          ...headers,
+          ...getToken()
+
+        }
+      })
+      .then(resp => resp.json())
+      .then(user => loginUser(user))
+    // setLoggedIn(true);
+  }
+}, [loggedIn])
 
   return (
     <Router>
-      { loggedIn ? <h1>we're loggedin</h1> : null }
-      <NavBar />
+      <NavBar loggedIn={loggedIn} logoutUser={ logoutUser } />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="signup" element={<Signup loginUser={ loginUser }/>} />
