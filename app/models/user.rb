94677621110499b2f7a2af56a00ad399e7a73ba6
kaye garcia <kayemgarcia@gmail.com>
2022-01-24 14:57:28 -0600

    Associated ingredients and user_ingredients to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password # password setter / encrypt_password / authenticate
+  has_many :ingredients
+  has_many :user_ingredients, through: :ingredients
+
+
 end
