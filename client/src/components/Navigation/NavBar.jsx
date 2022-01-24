b024ca400723e620bfe55d77623bbc0fdd828f47
kaye garcia <kayemgarcia@gmail.com>
2022-01-24 12:24:15 -0600

    Ternary for loggedOut and loggedIn links

--- client/src/components/Navigation/NavBar.jsx
+++ client/src/components/Navigation/NavBar.jsx
@@ -1,38 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = ({ loggedIn, logoutUser }) => {
 
-  const loggedOutLinks = () => {
-    return (
-      <ul>
-        <li><Link to="/">Homepage</Link></li>
-        <li><Link to="signup">Signup</Link></li>
-        <li><Link to="login">Login</Link></li>
-      </ul>
-    )
-  }
+
+const NavBar = ({ loggedIn, logoutUser, currentUser }) => {
+
+  // const loggedOutLinks = () => {
+  //   return (
+  //     <nav>
+  //       <Link to="/">Homepage</Link>
+  //       <Link to="signup">Signup</Link>
+  //       <Link to="login">Login</Link>
+  //     </nav>
+  //   )
+  // }
 
   const handleLogout = (event) => {
     event.preventDefault();
     logoutUser()
   }
 
-  const loggedInLinks = () => {
-    return (
-      <ul>
-        <li><Link to="/">Homepage</Link></li>
-        <li><a href="#home" onClick={ handleLogout }>Logout</a></li>
-      </ul>
-    )
-  }
+  // const loggedInLinks = () => {
+  //   return (
+  //     <ul>
+  //       <li><Link to="/">Homepage</Link></li>
+  //       <li><a href="#home" onClick={ handleLogout }>Logout</a></li>
+  //     </ul>
+  //   )
+  // }
 
   
   return (
-    <div>
-      { loggedIn ? loggedOutLinks() : loggedInLinks() }
+    <div id="navbar">
+      {loggedIn ? (
+        <>
+        <nav>
+          {/* <Link to="/"><GiMeal /></Link> */}
+          <Link to="/">Recipe Generator</Link>
+          <a href="#home" onClick={ handleLogout }>Logout</a>
+        </nav>
+        </>) : (
+        <>
+        <nav>
+          <Link to="/">Recipe Generator</Link>
+          <Link to="signup">Signup</Link>
+          <Link to="login">Login</Link>
+        </nav>
+        </>
+      )
+      }
     </div>
-  );
+  )
 };
 
 export default NavBar;
+
+// { loggedIn ? loggedInLinks() : loggedOutLinks() }
