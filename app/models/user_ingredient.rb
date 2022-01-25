f5f04e683a39c6e3565706b8037dbfbb2cac5366
kaye garcia <kayemgarcia@gmail.com>
2022-01-24 20:28:08 -0600

    Associate user_ingredients with ingredients

--- app/models/user_ingredient.rb
+++ app/models/user_ingredient.rb
@@ -1,4 +1,8 @@
 class UserIngredient < ApplicationRecord
   belongs_to :user
+  belongs_to :ingredients
   
 end
+
+
+# https://api.spoonacular.com/recipes/findByIngredients?apiKey=918beffc44fc41c396c5ed3e8f624650&ingredients=apples,+flour,+sugar&number=2
