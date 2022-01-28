e8d791bba5e3413899f99a91b1b8a6c0fe5f860a
kaye garcia <kayemgarcia@gmail.com>
2022-01-28 11:03:21 -0600

    Create array of categories

--- client/src/components/Ingredients/IngredientsContainer.jsx
+++ client/src/components/Ingredients/IngredientsContainer.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import IngredientsCategoryCard from "./IngredientsCategoryCard";
 
-const IngredientsContainer = () => {
-  return <div></div>;
-};
+const IngredientsContainer = ({ ingredients }) => {
+
+
+  const categories = [...new Set(ingredients.map(ingredient => ingredient.category))]
+
+
+  const renderCategoryCards = categories.map( category => {
+    const categoryIngredients = ingredients.filter(ingredient => ingredient.category === category)
+    return <IngredientsCategoryCard key={category} name={category} ingredients={categoryIngredients} />
+  })
+
+  
+
+  return (
+    <div>
+      { renderCategoryCards }
+    </div>
+  );
+}
 
 export default IngredientsContainer;
