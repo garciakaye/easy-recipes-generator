99efd6ac6202c4cc00e8d4797bad582f66a8aec9
kaye garcia <kayemgarcia@gmail.com>
2022-01-31 09:25:36 -0600

    Import reducer in store component

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userIngredientsReducer from "./components/Ingredients/userIngredientsSlice";
 
 // import restaurantsReducer from "./features/restaurants/restaurantsSlice";
 // import reviewsReducer from "./features/reviews/reviewsSlice";
 
 const store = configureStore({
   reducer: {
-    // restaurants: restaurantsReducer,
+    userIngredients: userIngredientsReducer,
     // reviews: reviewsReducer,
   },
 });
