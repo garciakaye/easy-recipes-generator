f4cb803ce303fd97a73299fc61defc18cd163fa6
kaye garcia <kayemgarcia@gmail.com>
2022-01-31 20:42:02 -0600

    Add user_ingredients attribute

--- app/serializers/user_ingredient_serializer.rb
+++ app/serializers/user_ingredient_serializer.rb
@@ -1,3 +1,5 @@
 class UserIngredientSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :ingredient_id
+  attributes :id, :user_id, :ingredient_id, :ingredient
+  # belongs_to :user
+  # belongs_to :ingredient
 end
