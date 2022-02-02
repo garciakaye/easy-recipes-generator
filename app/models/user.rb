054771839d78fec3cabdccf6967193109d40dfde
kaye garcia <kayemgarcia@gmail.com>
2022-02-02 14:43:19 -0600

    Comment out validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   has_many :user_ingredients
   has_many :ingredients, through: :user_ingredients
 
+  # validates :username, presence: true, uniqueness: { case_sensitive: false }
+  # validates :first_name, presence: true, case_sensitive: false 
+  # validates :last_name, presence: true, case_sensitive: false 
 
 end
