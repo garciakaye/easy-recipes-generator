4d0900fd84973436dc51057cd14066d52642a7c6
kaye garcia <kayemgarcia@gmail.com>
2022-02-01 19:24:44 -0600

    Remove includes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-    render json: @users, include: :user_ingredients
+    render json: @users
   end
 
   # GET /users/1
   def show
-    render json: @user, include: :user_ingredients
+    render json: @user
   end
 
   # POST /users
