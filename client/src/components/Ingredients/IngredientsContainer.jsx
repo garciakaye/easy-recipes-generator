58f57669daea37b0633694fb826031954f106727
kaye garcia <kayemgarcia@gmail.com>
2022-02-01 19:22:57 -0600

    Remove useState hook

--- client/src/components/Ingredients/IngredientsContainer.jsx
+++ client/src/components/Ingredients/IngredientsContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import IngredientsCategoryCard from "./IngredientsCategoryCard";
 
 const IngredientsContainer = ({ ingredients }) => {
