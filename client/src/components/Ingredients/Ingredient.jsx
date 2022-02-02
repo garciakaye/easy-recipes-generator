5a31253ff2596e8968bfb66e552e24626d4db46d
kaye garcia <kayemgarcia@gmail.com>
2022-02-01 19:23:40 -0600

    Remove useState hook

--- client/src/components/Ingredients/Ingredient.jsx
+++ client/src/components/Ingredients/Ingredient.jsx
@@ -1,25 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { userIngredientAdd, userIngredientsPost } from "./userIngredientsSlice";
+import { userIngredientsPost, userIngredientsDelete } from "./userIngredientsSlice";
 
 const Ingredient = ({ ingredient }) => {
   const user = useSelector((state) => state.user.entities[0])
+  const userIngredients = useSelector((state) => state.userIngredients.entities)
+  // console.log(userIngredients)
+
   const dispatch = useDispatch();
+  // debugger
+  const findUserIngredient = userIngredients.find(({ ingredient_id }) => ingredient_id === ingredient.id)
 
 
-  const handleAddUserIngredients = () => {
+  const handleUserIngredientAdd = () => {
     const userIngredient = {
       user_id: user.id,
       ingredient_id: ingredient.id
     }
     dispatch(userIngredientsPost(userIngredient))
   }
-  
+
+  const handleUserIngredientRemove = () => {
+    dispatch(userIngredientsDelete(findUserIngredient.id))
+  }
+
+
+  // const renderButton = findUserIngredient ?
+  //   <a href="#/" className="ingredients-btns-active" onClick={handleUserIngredientRemove}>
+  //     {ingredient.name} </a>
+  //   :
+  //   <a href="#/" className="ingredients-btns" onClick={handleUserIngredientAdd} >
+  //     {ingredient.name}
+  //   </a>
+
   return (
-  <a href="#" className="ingredients-btns" onClick={ handleAddUserIngredients} > 
-    {ingredient.name}
-  </a>
-    
+
+    <a href="#/" className="ingredients-btns-active" onClick={handleUserIngredientRemove}>
+      {ingredient.name} </a>
+
   );
 };
 
