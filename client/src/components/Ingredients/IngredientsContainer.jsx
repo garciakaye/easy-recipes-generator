878a659c0384133d8000909fd56ce9d141d2516c
kaye garcia <kayemgarcia@gmail.com>
2022-02-03 08:56:33 -0600

    Comment out async await fetch

--- client/src/components/Ingredients/IngredientsContainer.jsx
+++ client/src/components/Ingredients/IngredientsContainer.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import IngredientsCategoryCard from "./IngredientsCategoryCard";
+import { baseUrl } from "../../Globals";
 
-const IngredientsContainer = ({ ingredients }) => {
+const IngredientsContainer = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const response = await fetch(baseUrl + '/ingredients')
+  //       const results = await response.json();
+  //       console.log(results);
+  //       setIngredients(results);
+  //     }
+  //     catch (e) {
+  //       console.error(e);
+  //     }
+  //   }
+  //   fetchData();
+
+  // }, [])
+
+  useEffect(() => {
+    fetch(baseUrl + '/ingredients')
+      .then((r) => r.json())
+      .then((ingredient) => setIngredients(ingredient))
+  }, [])
+
 
   const categories = [...new Set(ingredients.map(ingredient => ingredient.category))]
 
