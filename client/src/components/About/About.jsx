c28bd99c99a8853cf1d34beb8c59f38c8cfaf825
kaye garcia <kayemgarcia@gmail.com>
2022-02-05 13:50:21 -0600

    Create and build About component

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import '../../styles/about.css';
+import LottieGirl from "../LottieFiles/LottieGirl";
+import AboutText from "./AboutText";
+
 
 const About = () => {
-  return (
-    <div>
 
+  return (
+    <div className="about-container">
+      <div className="about">
+        <div className="lottie-girl" >
+          <LottieGirl />
+        </div>
+        <div className="about-text" >
+          <AboutText />
+        </div>
+      </div>
     </div>
+
   );
 };
 
