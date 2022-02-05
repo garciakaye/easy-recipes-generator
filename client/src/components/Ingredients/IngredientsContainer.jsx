e58fa71ce997d5e30c8d3c51b5aaf4061a2b4a2d
kaye garcia <kayemgarcia@gmail.com>
2022-02-05 13:49:18 -0600

    Clean up code and empty spaces

--- client/src/components/Ingredients/IngredientsContainer.jsx
+++ client/src/components/Ingredients/IngredientsContainer.jsx
@@ -1,33 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import IngredientsCategoryCard from "./IngredientsCategoryCard";
-import { baseUrl } from "../../Globals";
 
-const IngredientsContainer = () => {
-  const [ingredients, setIngredients] = useState([]);
-
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const response = await fetch(baseUrl + '/ingredients')
-  //       const results = await response.json();
-  //       console.log(results);
-  //       setIngredients(results);
-  //     }
-  //     catch (e) {
-  //       console.error(e);
-  //     }
-  //   }
-  //   fetchData();
-
-  // }, [])
-
-  useEffect(() => {
-    fetch(baseUrl + '/ingredients')
-      .then((r) => r.json())
-      .then((ingredient) => setIngredients(ingredient))
-  }, [])
-
+const IngredientsContainer = ({ ingredients }) => {
 
   const categories = [...new Set(ingredients.map(ingredient => ingredient.category))]
 
@@ -40,7 +14,6 @@ const IngredientsContainer = () => {
       ingredients={categoryIngredients}
     />
   })
-
 
   return (
     <div>
