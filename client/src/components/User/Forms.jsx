3e40a5dead7a92780991d30a831cd95577e9bd26
kaye garcia <kayemgarcia@gmail.com>
2022-02-06 11:25:31 -0600

    Use useState for form toggle

--- client/src/components/User/Forms.jsx
+++ client/src/components/User/Forms.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/forms.css';
 import Signup from "./Signup";
 import Login from "./Login";
-import FoodLogo from "../LottieFiles/FoodLogo";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import FoodCarousel from "../LottieFiles/FoodCarousel";
 
-const Forms = ({ loggedIn, user }) => {
+const Forms = () => {
+  const [loginForm, setLoginForm] = useState(false)
+
+  const handleFormToggle = () => setLoginForm(!loginForm)
+
   return (
-    <div className="forms">
-      <div className="login-signup">
-        <Login loggedIn={loggedIn} user={user} />
-        <Signup />
-      </div>
-    </div>
+    <Container className="login-signup-container">
+      {loginForm ? (
+        <Card className="login-signup-card" style={{ height: '425px', width: '500px' }}>
+          <Card.Body>
+            <Card.Title>
+              <FoodCarousel />
+            </Card.Title>
+            <Card.Text>
+              <Login />
+              <a href="#/" onClick={handleFormToggle}>Not yet a member?</a>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ) : (
+        <Card className="login-signup-card" style={{ height: '425px', width: '500px' }}>
+          <Card.Body>
+            <Card.Title>
+              <FoodCarousel />
+            </Card.Title>
+            <Card.Text>
+              <Signup />
+              <a href="#/" onClick={handleFormToggle}>Already have an account?</a>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )
+      }
+    </Container>
   );
 };
 
