36881fa603496d0fc8d3155f0f0339266ec12004
kaye garcia <kayemgarcia@gmail.com>
2022-02-07 08:46:44 -0600

    Remove unused imports

--- client/src/components/User/Login.jsx
+++ client/src/components/User/Login.jsx
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { logInFetch } from "./userSlice";
-import Form from 'react-bootstrap/Form';
-import FoodCarousel from "../LottieFiles/FoodCarousel";
-
+import { userIngredientsGet } from "../Ingredients/userIngredientsSlice";
 
 
 const Login = () => {
   const loggedIn = useSelector(state => state.user.loggedIn)
+  const userIngredients = useSelector((state) => state.userIngredients.entities)
 
   const navigate = useNavigate();
 
@@ -36,47 +35,43 @@ const Login = () => {
       ...formData
     }
     dispatch(logInFetch(strongParams))
+    dispatch(userIngredientsGet(userIngredients))
   }
 
 
-
   return (
-    <div className="form-container sign-in-container">
-      <form
-        id="login"
-        onSubmit={handleSubmit}
+    <form
+      id="login"
+      onSubmit={handleSubmit}
+    >
+      <div>
+        <label htmlFor="username"></label>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password"></label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
+      <button
+        className="login-btn"
+        type="submit"
+        value="Login"
       >
-        <h5>Already have an account?</h5>
-        <h6>Sign In</h6>
-        <div>
-          <label htmlFor="username"></label>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password"></label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </div>
-        <button
-          className="login-btn"
-          type="submit"
-          value="Login"
-        >
-          Login
-        </button>
-      </form>
-    </div>
+        Login
+      </button>
+    </form>
   );
 };
 
