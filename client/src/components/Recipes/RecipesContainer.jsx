58ea4f17356ee0690c3460b3dae37d6cbfe4745e
kaye garcia <kayemgarcia@gmail.com>
2022-02-07 08:47:14 -0600

    Begin to build RecipesContainer

--- client/src/components/Recipes/RecipesContainer.jsx
+++ client/src/components/Recipes/RecipesContainer.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from "react-redux";
+import { apiHeaders } from "../../Globals";
 
 const RecipesContainer = () => {
+  const user = useSelector((state) => state.user.entities[0])
+  const userIngredients = useSelector((state) => state.userIngredients.entities)
+  // console.log(user.ingredients)
+
+  // const userIngredientsNames = user.ingredients.map(name => console.log(name))
+  // console.log(userIngredientsNames)
+
+  const [recipes, setRecipes] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=onions%2C%20celery%2C%20carrots&number=5&ignorePantry=true&ranking=1", {
+      "method": "GET",
+      "headers": apiHeaders
+    })
+      .then(response => response.json())
+      .then(recipes => console.log(recipes))
+
+  }, [])
+
+
+
   return (
     <div>
 
