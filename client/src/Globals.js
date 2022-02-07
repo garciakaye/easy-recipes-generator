83d942680708c05818fe6fad2f1f28fbf8eb75a4
kaye garcia <kayemgarcia@gmail.com>
2022-02-07 08:47:48 -0600

    Clean code and remove empty spaces

--- client/src/Globals.js
+++ client/src/Globals.js
@@ -1,3 +1,5 @@
+
+
 export const baseUrl = "http://localhost:3000";
 export const headers = {
   "Accept": "application/json",
@@ -6,6 +8,13 @@ export const headers = {
 
 export const getToken = () => {
   return {
-    'Authorization': `bearer ${ localStorage.getItem('jwt') }`
+    'Authorization': `bearer ${localStorage.getItem('jwt')}`
   }
+}
+
+export const apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=onion%2Ccelery%2Ccarrots&number=5&ignorePantry=true&ranking=1"
+export const apiHeaders = {
+
+  "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+  "x-rapidapi-key": "f6b3957100msh997ff790bf6be7bp164369jsn3534030ce568"
 }
