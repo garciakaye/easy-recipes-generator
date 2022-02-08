f60c3a48eacf01776c4d36bc84a8b86f359d30b6
kaye garcia <kayemgarcia@gmail.com>
2022-02-08 09:31:12 -0600

    Commit changes

--- client/src/Globals.js
+++ client/src/Globals.js
@@ -10,12 +10,3 @@ export const getToken = () => {
   }
 }
 
-<<<<<<< HEAD
-=======
-export const apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=onion%2Ccelery%2Ccarrots&number=5&ignorePantry=true&ranking=1"
-export const apiHeaders = {
-
-  "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-  "x-rapidapi-key": "f6b3957100msh997ff790bf6be7bp164369jsn3534030ce568"
-}
->>>>>>> parent of b39c689 (Remove exposed API key)
