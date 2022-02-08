dcb4bca1f7c1c019171b8f9034e937659226f30c
kaye garcia <kayemgarcia@gmail.com>
2022-02-08 08:53:52 -0600

    Remove commit b39c689

--- client/src/Globals.js
+++ client/src/Globals.js
@@ -6,12 +6,6 @@ export const headers = {
 
 export const getToken = () => {
   return {
-    'Authorization': `bearer ${ localStorage.getItem('jwt') }`
+    'Authorization': `bearer ${localStorage.getItem('jwt')}`
   }
-<<<<<<< HEAD
 }
-
-export const apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=onion%2Ccelery%2Ccarrots&number=5&ignorePantry=true&ranking=1"
-=======
-}
->>>>>>> parent of 83d9426 (Clean code and remove empty spaces)
