df7a3a8f3dde0c74b3c71b2e015255683940a5e6
kaye garcia <kayemgarcia@gmail.com>
2022-02-08 07:54:44 -0600

    Remove empty space

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,9 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
-    render json: @users
-  end
+		@users = User.all
+		render json: @users
+	end
 
   # GET /users/1
   def show
