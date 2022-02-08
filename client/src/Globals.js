1c3a0317f830028b24bbcbf382bddf6acca6cd03
kaye garcia <kayemgarcia@gmail.com>
2022-02-08 09:03:47 -0600

    Commit changes

--- client/src/Globals.js
+++ client/src/Globals.js
@@ -12,9 +12,4 @@ export const getToken = () => {
   }
 }
 
-export const apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=onion%2Ccelery%2Ccarrots&number=5&ignorePantry=true&ranking=1"
-export const apiHeaders = {
 
-  "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-  "x-rapidapi-key": "f6b3957100msh997ff790bf6be7bp164369jsn3534030ce568"
-}
