d6d2bb9fac11ef14b0a20c8692ef9af941434088
kaye garcia <kayemgarcia@gmail.com>
2022-02-08 08:48:38 -0600

    Resolve merge conflict

--- client/src/Globals.js
+++ client/src/Globals.js
@@ -1,5 +1,3 @@
-
-
 export const baseUrl = "http://localhost:3000";
 export const headers = {
   "Accept": "application/json",
@@ -8,8 +6,12 @@ export const headers = {
 
 export const getToken = () => {
   return {
-    'Authorization': `bearer ${localStorage.getItem('jwt')}`
+    'Authorization': `bearer ${ localStorage.getItem('jwt') }`
   }
+<<<<<<< HEAD
 }
 
 export const apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=onion%2Ccelery%2Ccarrots&number=5&ignorePantry=true&ranking=1"
+=======
+}
+>>>>>>> parent of 83d9426 (Clean code and remove empty spaces)
