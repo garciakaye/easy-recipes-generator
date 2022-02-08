a1d635af8afe3d7a22328f7c9f73c9c033a954bc
kaye garcia <kayemgarcia@gmail.com>
2022-02-08 07:54:20 -0600

    Remove ingredient attribute

--- app/serializers/user_ingredient_serializer.rb
+++ app/serializers/user_ingredient_serializer.rb
@@ -1,5 +1,6 @@
 class UserIngredientSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :ingredient_id, :ingredient
-  # belongs_to :user
-  # belongs_to :ingredient
+  attributes :id, :user_id, :ingredient_id
+  # :ingredient
+#   belongs_to :user
+#   belongs_to :ingredient
 end
