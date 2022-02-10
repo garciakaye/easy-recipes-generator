782cacfc5e3068983e65350b428815bda6cf5bd1
kaye garcia <kayemgarcia@gmail.com>
2022-02-09 20:04:46 -0600

    Create Recipes component

---
+++ client/src/components/Recipes/Recipes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RecipesCard from "./RecipesCard";
+
+const Recipes = ({ recipes }) => {
+  // console.log("hi")
+  // const recipeIds = recipes.map(recipe => console.log(recipe.id))
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Recipes;
