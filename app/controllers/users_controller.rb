4b36987a8c2c04ccd25d70dd51e0bb55fc170f6f
kaye garcia <kayemgarcia@gmail.com>
2022-02-09 20:09:40 -0600

    Test permitting :user_ingredient

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :username, :password, :user_ingredients)
+      params.require(:user).permit(:first_name, :last_name, :username, :password, :user_ingredients, :user_ingredient)
     end
 end
