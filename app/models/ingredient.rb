a2b1c03ce44ce89959a6a9ef720660ab04af4078
kaye garcia <kayemgarcia@gmail.com>
2022-02-09 20:09:03 -0600

    Remove test method

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,8 @@ class Ingredient < ApplicationRecord
   has_many :user_ingredients
   has_many :users, through: :user_ingredients
 
+  def self.category_desc
+    all.order(category: :desc, name: :asc)
+  end
 
 end
