7dfcda701647e867823e367e8cc672ce7f6d1bf9
kaye garcia <kayemgarcia@gmail.com>
2022-02-09 20:03:29 -0600

    Build recipeCard component

--- client/src/components/Recipes/RecipesCard.jsx
+++ client/src/components/Recipes/RecipesCard.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { useSelector } from "react-redux";
 
-const RecipesCard = () => {
+const RecipesCard = ({ recipe }) => {
+
+  const handleClick = () => {
+    fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/informationBulk?ids=${recipe.id}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY
+      }
+    })
+      .then(response => response.json())
+      .then(res => console.log(res))
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   return (
-    <div>
-
-    </div>
+    <Card className="recipes-card" style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={recipe.image} />
+      <Card.Body>
+        <Card.Title>{recipe.title}</Card.Title>
+        <Card.Text>
+        </Card.Text>
+        {/* <Button variant="primary">Go somewhere</Button> */}
+      </Card.Body>
+    </Card >
   );
 };
 
