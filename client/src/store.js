afd6b6b5203f5a236d404591449b88bece23cde6
kaye garcia <kayemgarcia@gmail.com>
2022-02-09 20:07:56 -0600

    Add recipe reducer to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userIngredientsReducer from "./components/Ingredients/userIngredientsSlice";
 import userReducer from "./components/User/userSlice";
+import recipesReducer from "./components/Recipes/recipesSlice";
 
 
 const store = configureStore({
   reducer: {
     userIngredients: userIngredientsReducer,
-    user: userReducer
+    user: userReducer,
+    recipes: recipesReducer
   },
 });
 
