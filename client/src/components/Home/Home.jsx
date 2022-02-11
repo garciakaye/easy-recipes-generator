e25783c970ca0f5252a3994a283cc6460c8a135f
kaye garcia <kayemgarcia@gmail.com>
2022-02-11 10:48:54 -0600

    Clean code and remove unused imports

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/home.css';
-// import { baseUrl } from "../../Globals";
 import { Row, Col } from "react-bootstrap";
 import IngredientsContainer from "../Ingredients/IngredientsContainer";
 import RecipesContainer from "../Recipes/RecipesContainer";
 import { recipesGet } from "../Recipes/recipesSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+
 
 const Home = () => {
-  const dispatch = useDispatch();
+  // const recipes = useSelector((state) => state.recipes.entities)
+
+
+  // const dispatch = useDispatch();
+
+  // dispatch(recipesGet(recipes))
+
+
+
   return (
     <div className="home-container"  >
       Homepage
@@ -19,7 +27,7 @@ const Home = () => {
         </Col>
         <Col className="border" xs={3}>
           My Pantry
-          {/* <IngredientsContainer ingredients={ingredients} /> */}
+
           <IngredientsContainer />
         </Col>
       </Row>
