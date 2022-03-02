655e64288caa28a7e21a79aaaf1485f514c223cb
kaye garcia <kayemgarcia@gmail.com>
2022-03-02 13:08:40 -0600

    Comment useIngredientsSlice

--- client/src/components/Ingredients/userIngredientsSlice.jsx
+++ client/src/components/Ingredients/userIngredientsSlice.jsx
@@ -1,38 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
 
 
-const userIngredientsSlice = createSlice({
-  name: "userIngredients",
-  initialState: {
-    entities: [],
-    ingredients: [],
-    status: 'idle',
-  },
-  reducers: {
-    userIngredientAdd(state, action) {
-      state.entities.push({
-        user_id: action.payload.user_id,
-        ingredient_id: action.payload.ingredient_id,
-      });
-    },
-    userIngredientsGet(state, action) {
-      state.entities = action.payload
-    },
-    userIngredientsName(state, action) {
-      state.ingredients = action.payload
-    },
-    userIngredientRemove(state, action) {
-      if (state.entities.find(ingred => ingred.id === action.payload)) {
-        state.entities = state.entities.filter(ingred => ingred.id !== action.payload)
-      }
-    },
-  },
-});
+// const userIngredientsSlice = createSlice({
+//   name: "userIngredients",
+//   initialState: {
+//     entities: [],
+//     ingredients: [],
+//     status: 'idle',
+//   },
+//   reducers: {
+//     userIngredientAdd(state, action) {
+//       state.entities.push({
+//         user_id: action.payload.user_id,
+//         ingredient_id: action.payload.ingredient_id,
+//       });
+//     },
+//     userIngredientsGet(state, action) {
+//       state.entities = action.payload
+//     },
+//     userIngredientsName(state, action) {
+//       state.ingredients = action.payload
+//     },
+//     userIngredientRemove(state, action) {
+//       if (state.entities.find(ingred => ingred.id === action.payload)) {
+//         state.entities = state.entities.filter(ingred => ingred.id !== action.payload)
+//       }
+//     },
+//   },
+// });
 
-export const { userIngredientsName, userIngredientAdd, userIngredientRemove, userIngredientsGet } = userIngredientsSlice.actions;
+// export const { userIngredientsName, userIngredientAdd, userIngredientRemove, userIngredientsGet } = userIngredientsSlice.actions;
 
-export default userIngredientsSlice.reducer;
+// export default userIngredientsSlice.reducer;
 
 
 
