2c8ef02d298dfd9b1ef715e90933e162eec0de77
kaye garcia <kayemgarcia@gmail.com>
2022-03-02 13:11:15 -0600

    Add middleware in store

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userIngredientsReducer from "./components/Ingredients/userIngredientsSlice";
 import userReducer from "./components/User/userSlice";
+import errorsReducer from './errorHandling/errorsSlice';
 import recipesReducer from "./components/Recipes/recipesSlice";
 
 
@@ -8,8 +9,10 @@ const store = configureStore({
   reducer: {
     userIngredients: userIngredientsReducer,
     user: userReducer,
+    errors: errorsReducer,
     recipes: recipesReducer
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 export default store;
