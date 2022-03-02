700709d087cd4a7961b4a4aa63370ae0a7bb1c61
kaye garcia <kayemgarcia@gmail.com>
2022-03-02 13:06:43 -0600

    Comment recipesSlice

--- client/src/components/Recipes/recipesSlice.jsx
+++ client/src/components/Recipes/recipesSlice.jsx
@@ -1,23 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import api from "../../api";
 
 
-const recipesSlice = createSlice({
-  name: 'recipes',
-  initialState: {
-    entities: [],
-    status: 'idle',
-  },
-  reducers: {
-    recipesGet(state, action) {
-      state.entities = action.payload
-    },
-    recipesLoading(state, action) {
-      state.status = "loading"
-    },
-  },
-});
+// export const fetchRecipeByIngredients = createAsyncThunk(
+//   'recipes/fetchRecipesByIngredients',
+//   async (ingredients, thunkAPI) => {
+//     const response = await api.recipes.getRecipesByIngredients(ingredients)
+//     return response.data
+//   }
+// )
 
-// export the action creators
-export const { recipesGet } = recipesSlice.actions;
 
-export default recipesSlice.reducer;
+
+
+// const recipesSlice = createSlice({
+//   name: 'recipes',
+//   initialState: {
+//     entities: [],
+//     status: 'idle',
+//   },
+//   reducers: {
+//     recipesGet(state, action) {
+//       state.entities = action.payload
+//     },
+//     recipesLoading(state, action) {
+//       state.status = "loading"
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder.addCase(fetchRecipeByIngredients.fulfilled, (state, action) => {
+      
+//     })
+//   }
+// });
+
+
+// export const { recipesGet } = recipesSlice.actions;
+
+// export default recipesSlice.reducer;
