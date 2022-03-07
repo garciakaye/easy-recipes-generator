9b671329f491bf0409984860f266e2cdf3038fc4
kaye garcia <kayemgarcia@gmail.com>
2022-03-07 13:00:30 -0600

    Dispatch getAllIngredients

--- client/src/components/Ingredients/IngredientsContainer.jsx
+++ client/src/components/Ingredients/IngredientsContainer.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import IngredientsCategoryCard from "./IngredientsCategoryCard";
+import { getAllIngredients } from "./IngredientsSlice";
+import { useDispatch, useSelector } from "react-redux";
 
-const IngredientsContainer = ({ allIngredients }) => {
+
+const IngredientsContainer = () => {
+  const ingredients = useSelector((state) => state.ingredients.items)
+
+  const dispatch = useDispatch()
 
 
-  const categories = [...new Set(allIngredients.map(ingredient => ingredient.category))]
+  useEffect(() => {
+    dispatch(getAllIngredients())
+  }, [dispatch])
+
+  const hasLoadedIngredients = ingredients.length > 0
+
+
+  if (!hasLoadedIngredients) {
+    //or some other type of placeholder
+    return null
+  }
+
+
+  const categories = [...new Set(ingredients.map(ingredient => ingredient.category))]
+
 
   const renderCategoryCards = categories.map(category => {
-    const categoryIngredients = allIngredients.filter(ingredient => ingredient.category === category)
+    const categoryIngredients = ingredients.filter(ingredient => ingredient.category === category)
     return <IngredientsCategoryCard
       key={category}
       name={category}
