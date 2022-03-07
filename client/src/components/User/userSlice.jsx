12d851f116eb1db4e5a58229b833a3990825f2b2
kaye garcia <kayemgarcia@gmail.com>
2022-03-07 12:58:08 -0600

    Clean code and remove unused functions

--- client/src/components/User/userSlice.jsx
+++ client/src/components/User/userSlice.jsx
@@ -3,7 +3,70 @@ import { headers, getToken } from "../../Globals";
 import { setErrors } from "../../errorHandling/errorsSlice";
 
 
-export function logInFetch(strongParams) {
+const initialState = {
+  id: -1,
+  firstName: '',
+  lastName: '',
+  username: '',
+  isLoggedIn: false,
+  status: 'idle',
+  ingredientIds: []
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    userLoginLoading(state, action) {
+      state.status = "loading"
+    },
+    userFetchRejected(state, action) {
+      state.status = "rejected"
+    },
+    setUser(state, action) {
+      const { id, firstName, lastName, username } = action.payload
+      state = {
+        id,
+        firstName,
+        lastName,
+        username,
+        status: 'succeeded',
+        isLoggedIn: true,
+        errors: []
+      }
+      return state
+    },
+    clearUser(state, action) {
+      return initialState
+    },
+    ingredientAdd(state, action) {
+      state.ingredientIds.push({
+        user_id: action.payload.user_id,
+        ingredient_id: action.payload.ingredient_id
+      })
+    },
+    ingredientRemove(state, action) {
+      if (state.ingredientIds.find(ingred => ingred.id === action.payload)) {
+        state.ingredientIds = state.ingredientIds.filter(ingred => ingred.id !== action.payload)
+      }
+    },
+    setUserIngredients(state, action) {
+      state.ingredientIds = action.payload
+      state.status = 'idle'
+    }
+  },
+});
+
+// export the action creators
+export const {
+  userFetchRejected,
+  usersFetched,
+  setUser,
+  clearUser,
+  setUserIngredients
+} = userSlice.actions;
+
+export function login(strongParams) {
   return function (dispatch) {
     dispatch({ type: "users/userLoginLoading" });
     fetch('/login', {
@@ -19,9 +82,7 @@ export function logInFetch(strongParams) {
           res.json()
             .then(data => {
               localStorage.setItem('jwt', data.token)
-              dispatch(userLogInFetch(data.user))
-              dispatch(setErrors([]))
-              dispatch(userFetchSucceeded())
+              dispatch(setUser(data))
             })
         } else {
           res.json().then(errors => {
@@ -32,6 +93,23 @@ export function logInFetch(strongParams) {
       })
   }
 }
+
+export function signup(user) {
+  return (dispatch) => {
+    //dispatch signing up status
+    fetch('/users', {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(user)
+    }).then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem("jwt", data.token)
+        dispatch(setUser(data.user))
+      })
+
+  }
+}
+
 
 export function verifyLoggedIn() {
   return function (dispatch) {
@@ -47,9 +125,7 @@ export function verifyLoggedIn() {
         if (res.ok) {
           res.json()
             .then(data => {
-              dispatch(userLogInFetch(data))
-              dispatch(setErrors([]))
-              dispatch(userFetchSucceeded())
+              dispatch(setUser(data))
             })
         } else {
           res.json().then(errors => {
@@ -61,88 +137,32 @@ export function verifyLoggedIn() {
   }
 }
 
+export function getUserIngredients() {
+  return (dispatch) => {
+    dispatch({ type: "userIngredients/userIngredientsLoading" });
+    fetch('/user_ingredients')
+      .then((response) => response.json())
+      .then((ingredients) => dispatch(setUserIngredients(ingredients)))
+  }
+}
 
-
-
-
-// export function logInFetch(strongParams) {
-//   return function (dispatch) {
-
-//     dispatch({ type: "users/userLoginLoading" });
-//     fetch(baseUrl + '/login', {
-//       method: "POST",
-//       headers,
-//       body: JSON.stringify(strongParams)
-//     })
-//       .then(res => {
-//         if (res.ok) {
-//           res.json()
-//             .then(data => {
-//               localStorage.setItem('jwt', data.token)
-//               dispatch(userLoggedIn(data.user))
-//               // dispatch(userIngredientsGet(data.user_ingredients))
-//               // dispatch(userIngredientsName(data.ingredients))
-//               // dispatch(ingredientsFetched(data.all_ingredients))
-
-//             })
-//         } else {
-//           res.json().then(errors => dispatch(setErrors(errors)))
-//         }
-//       })
+// const handleUserIngredientAdd = () => {
+//   const userIngredient = {
+//     user_id: user.id,
+//     ingredient_id: ingredient.id
 //   }
+//   fetch(baseUrl + "/user_ingredients", {
+//     method: "POST",
+//     headers,
+//     body: JSON.stringify(userIngredient)
+//   })
+//     .then(res => res.json())
+//     .then(data => {
+//       setFindUserIngredient(data)
+//       dispatch(userIngredientAdd(data))
+//     })
 // }
 
-
-const userSlice = createSlice({
-  name: 'user',
-  initialState: {
-    entities: [],
-    status: 'idle',
-  },
-  reducers: {
-    userLoginLoading(state, action) {
-      state.status = "loading"
-    },
-    userFetchRejected(state, action) {
-      state.status = "rejected"
-    },
-    userFetchSucceeded(state, action) {
-      state.status = "succeeded"
-    },
-    usersFetched(state, action) {
-      state.entities = action.payload;
-    },
-    userLogInFetch(state, action) {
-      state.entities = [...state.entities, {
-        id: action.payload.id,
-        first_name: action.payload.first_name,
-        last_name: action.payload.last_name,
-        username: action.payload.username,
-        password: action.payload.password
-      }];
-    },
-    userLoggedIn(state, action) {
-      state.loggedIn = action.payload
-    },
-    userLogout(state, action) {
-      state.entities = action.payload
-    },
-    userLogoutStatus(state, action) {
-      state.status = 'idle'
-    },
-  },
-});
-
-// export the action creators
-export const {
-  userLoggedIn,
-  userLogout,
-  userLogoutStatus,
-  userFetchSucceeded,
-  userFetchRejected,
-  usersFetched,
-  userLogInFetch
-} = userSlice.actions;
 
 export default userSlice.reducer;
 
