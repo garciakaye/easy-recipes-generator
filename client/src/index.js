62f7871bbae7f21d186f3a2be30f0f5a55b06a15
kaye garcia <kayemgarcia@gmail.com>
2022-03-09 16:39:05 -0600

    Remove comments

--- client/src/index.js
+++ client/src/index.js
@@ -7,19 +7,6 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import store from './store';
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from "react-redux";
-// import { createStore, applyMiddleware } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import './index.css';
-// import App from './App';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import reportWebVitals from './reportWebVitals';
-// import rootReducer from './reducers';
-// import thunk from 'redux-thunk'
-
-// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
