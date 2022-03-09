853b878b6cd0bfad46ce84e94a4872d1e3992c7c
kaye garcia <kayemgarcia@gmail.com>
2022-03-09 16:36:02 -0600

    Update signup import from features folder

--- client/src/components/User/Signup.jsx
+++ client/src/components/User/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { signup } from "./userSlice";
+import { signup } from "../../features/userSlice";
 import { useNavigate } from "react-router-dom";
 
 
