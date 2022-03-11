6f7a9a838301049159ab3bf6f9729459dcbe8e7b
kaye garcia <kayemgarcia@gmail.com>
2022-03-11 17:53:57 -0600

    Render shopping list function

--- client/src/components/User/Profile.jsx
+++ client/src/components/User/Profile.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div></div>;
+  const shoppingList = useSelector((state) => state.user.shoppingList)
+
+
+  const renderShoppingList = () => {
+    return shoppingList.map(item => {
+      return <li key={item}>
+        {item}
+      </li>
+    })
+  }
+
+
+  return (
+    <ul>
+      {renderShoppingList()}
+    </ul>);
 };
 
 export default Profile;
