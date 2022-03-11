6a6fe9d291929c4e4c4b827caa56b652e31e5919
kaye garcia <kayemgarcia@gmail.com>
2022-03-11 17:57:08 -0600

    Comment recipesReducer

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import ingredientsReducer from "./features/IngredientsSlice";
 import userReducer from "./features/userSlice";
 import errorsReducer from './features/errorsSlice';
-import recipesReducer from "./features/recipesSlice";
+// import recipesReducer from "./features/recipesSlice";
 
 
 const store = configureStore({
@@ -10,7 +10,7 @@ const store = configureStore({
     ingredients: ingredientsReducer,
     user: userReducer,
     errors: errorsReducer,
-    recipes: recipesReducer
+    // recipes: recipesReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
