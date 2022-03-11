44f69769b717a4bf42ebed5afff785c12e025422
kaye garcia <kayemgarcia@gmail.com>
2022-03-11 17:57:38 -0600

    Associate shopping list

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password # password setter / encrypt_password / authenticate
   has_many :user_ingredients
   has_many :ingredients, through: :user_ingredients
+  has_one :shopping_list 
 
   # validates :username, presence: true, uniqueness: { case_sensitive: false }
   # validates :first_name, presence: true, case_sensitive: false 
