64e3837906045380d8d5d9d646063c6aef296f1d
kaye garcia <kayemgarcia@gmail.com>
2022-03-14 15:58:13 -0500

    Add login input in navbar login drop down

--- client/src/components/Navigation/NavBar.jsx
+++ client/src/components/Navigation/NavBar.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GiMeal } from "react-icons/gi";
 import { CgProfile } from "react-icons/cg";
 import Dropdown from 'react-bootstrap/Dropdown';
-import Login from "../User/Login";
 import { useSelector, useDispatch } from "react-redux";
 import { clearUser } from "../../features/userSlice";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../features/userSlice";
 
 
 const NavBar = () => {
   const isLoggedIn = useSelector(state => state.user.isLoggedIn)
   const username = useSelector(state => state.user.username)
 
+  let navigate = useNavigate()
   const dispatch = useDispatch()
 
+  const [formData, setFormData] = useState({
+    username: "",
+    password: ""
+  })
+
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+
+  const handleLogInSubmit = e => {
+    e.preventDefault();
+    dispatch(login(formData))
+    navigate('/home')
+  }
 
   const handleLogOut = () => {
     localStorage.removeItem('jwt')
@@ -27,11 +46,15 @@ const NavBar = () => {
           <nav>
             <Link to="/"><GiMeal /></Link>
             <Link className="link" to="/home">Easy Recipes Generator</Link>
-            <Link className="link" to="/about">About</Link>
-            <Link className="link" to="/" onClick={handleLogOut}>Logout</Link>
+            <Link className="link" to="/shopping-list">Shopping List</Link>
           </nav>
           <nav>
-            <Link to="/profile"><CgProfile />{username}</Link>
+            <Dropdown>
+              <Dropdown.Toggle id="login-dropdown"><CgProfile />{username}</Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Link className="link" to="/" onClick={handleLogOut}>Logout</Link>
+              </Dropdown.Menu>
+            </Dropdown>
           </nav>
         </>
       ) : (
@@ -42,7 +65,30 @@ const NavBar = () => {
           <Dropdown>
             <Dropdown.Toggle id="login-dropdown">Login</Dropdown.Toggle>
             <Dropdown.Menu>
-              <Login />
+              <label htmlFor="username"></label>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+              />
+              <label htmlFor="password"></label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+              <button
+                className="dd-login-btn"
+                type="submit"
+                value="Login"
+                onClick={handleLogInSubmit}
+              >
+                Login
+              </button>
             </Dropdown.Menu>
           </Dropdown>
         </>
