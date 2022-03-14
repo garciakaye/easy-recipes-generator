712ab4dc57ce265197726f621c44c22bec0575f9
kaye garcia <kayemgarcia@gmail.com>
2022-03-14 15:57:53 -0500

    Add p tag

--- client/src/components/Recipes/RecipesCard.jsx
+++ client/src/components/Recipes/RecipesCard.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { CgExternal } from "react-icons/cg";
 import { shoppingListAdd } from "../../features/userSlice";
 import { useDispatch } from "react-redux";
 
 const RecipesCard = ({ recipe }) => {
-
   const dispatch = useDispatch();
 
 
@@ -21,10 +20,11 @@ const RecipesCard = ({ recipe }) => {
       </Card.Header>
       <Card.Img variant="top" src={recipe.image} />
       <Card.Body>
+        <p>Add the {recipe.missedIngredientCount} missing ingredients to shopping list</p>
         {recipe.missedIngredients.map((ingredient, index) => {
-          return <Card.Text className="missed-ingredients" key={index} onClick={() => addToShoppingList(ingredient.name)}>
-            {ingredient.name}
-          </Card.Text>
+          return <ul className="recipe-ul" key={index}>
+            <a className="missed-ingredients" href="#/" onClick={() => addToShoppingList(ingredient.name)}>{ingredient.name}</a>
+          </ul>
         })}
       </Card.Body>
     </Card >
