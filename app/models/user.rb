978ab0d0c2b5826c814ace13fe9a084d66ee3fbd
kaye garcia <kayemgarcia@gmail.com>
2022-03-16 17:29:08 -0500

    Add login/signup validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :ingredients, through: :user_ingredients
   has_one :shopping_list 
 
-  # validates :username, presence: true, uniqueness: { case_sensitive: false }
-  # validates :first_name, presence: true, case_sensitive: false 
-  # validates :last_name, presence: true, case_sensitive: false 
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, on: :create
+  validates :first_name, :last_name, presence: true, uniqueness: { case_sensitive: false }, on: :create
+  
 
 end
