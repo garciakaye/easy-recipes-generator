2ef2fdc7c84f10282b2746c70e78bf67460e6827
kaye garcia <kayemgarcia@gmail.com>
2022-03-16 17:23:25 -0500

    Import ErrorAlert component

--- client/src/components/User/Login.jsx
+++ client/src/components/User/Login.jsx
@@ -3,11 +3,12 @@ import '../../styles/forms.css';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../features/userSlice";
-import Form from 'react-bootstrap/Form'
-
+import Form from 'react-bootstrap/Form';
+import ErrorAlert from "../../errorHandling/ErrorAlert";
 
 
 const Login = () => {
+  const errors = useSelector(state => state.errors.entities[0])
 
   const isLoggedIn = useSelector(state => state.user.isLoggedIn)
 
@@ -35,33 +36,35 @@ const Login = () => {
   const handleLogInSubmit = e => {
     e.preventDefault();
     dispatch(login(formData))
-    navigate('/home')
   }
 
   return (
-    <Form className="login-form" onSubmit={handleLogInSubmit}>
-      <Form.Group>
-        <Form.Label htmlFor="username"></Form.Label>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label htmlFor="password"></Form.Label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <input className="login-btn" type="submit" value="Login" />
-    </Form>
+    <div>
+      <Form className="login-form" onSubmit={handleLogInSubmit}>
+        {errors ? <ErrorAlert errors={errors} /> : null}
+        <Form.Group>
+          <Form.Label htmlFor="username"></Form.Label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="password"></Form.Label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <input className="login-btn" type="submit" value="Login" />
+      </Form>
+    </div>
   );
 };
 
