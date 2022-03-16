c37cc08a8b39530e9057342425a930070baa107c
kaye garcia <kayemgarcia@gmail.com>
2022-03-16 17:24:55 -0500

    Import CircularColor and add to return ternary

--- client/src/components/Recipes/RecipesContainer.jsx
+++ client/src/components/Recipes/RecipesContainer.jsx
@@ -3,56 +3,88 @@ import { useSelector } from "react-redux";
 import RecipesCard from "./RecipesCard";
 import { apiUrlFindByIngredients } from "../../Globals";
 import ReactPaginate from 'react-paginate';
+import CircularColor from "../Progress/CircularColor";
 
 
 
 const RecipesContainer = () => {
+
+  const itemsPerPage = 10;
+
   const myIngredients = useSelector((state) => state.user.ingredients);
 
-  const [recipeList, setRecipeList] = useState({})
+
+  const [recipeList, setRecipeList] = useState({});
+
+  const [currentList, setCurrentList] = useState([]);
+
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const [pageCount, setPageCount] = useState(0);
+
 
 
   useEffect(() => {
-    if (myIngredients.length > 0) {
-      const userIngredientNamesArray = myIngredients.map(ingredient => ingredient.name)
-      const getRecipes = async () => {
-        const response = await fetch(apiUrlFindByIngredients + userIngredientNamesArray, {
-          "method": "GET",
-          "headers": {
-            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-            "x-rapidapi-key": process.env.REACT_APP_API_KEY
-          }
-        })
-
-        let recipeData = await response.json()
-        setRecipeList(recipeData)
-      }
-      getRecipes()
+    if (recipeList.results) {
+      const endOffset = itemOffset + itemsPerPage;
+      const items = recipeList.results;
+      setCurrentList(items.slice(itemOffset, endOffset));
+      setPageCount(Math.ceil(items.length / itemsPerPage));
     }
+  }, [itemOffset, recipeList.results]);
+
+  useEffect(() => {
+
+    const userIngredientNamesArray = myIngredients.map(ingredient => ingredient.name)
+    const getRecipes = async () => {
+      const response = await fetch(apiUrlFindByIngredients + userIngredientNamesArray, {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+          "x-rapidapi-key": process.env.REACT_APP_API_KEY
+        }
+      })
+
+      let recipeData = await response.json()
+      setRecipeList(recipeData)
+    }
+    getRecipes()
+
   }, [myIngredients])
 
 
 
   const renderRecipeCards = () => {
-    return recipeList.results.map(recipe => {
+    return currentList.map(recipe => {
       return <RecipesCard key={recipe.id} recipe={recipe}></RecipesCard>
     })
   }
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % recipeList.results.length;
+    setItemOffset(newOffset);
+  };
+
 
 
 
   return (
     <div className="recipes-container">
-      {recipeList.results ? renderRecipeCards() : null}
-      {/* <ReactPaginate
+      {recipeList.results ? renderRecipeCards() : <CircularColor />}
+      <ReactPaginate
         breakLabel="..."
         nextLabel="next >"
-        // onPageChange={handlePageClick}
+        onPageChange={handlePageClick}
+        containerClassName="pagination"
         pageRangeDisplayed={5}
-        // pageCount={pageCount}
+        pageCount={pageCount}
         previousLabel="< previous"
         renderOnZeroPageCount={null}
-      /> */}
+        previousLinkClassName={"pagination__link"}
+        nextLinkClassName={"pagination__link"}
+        disabledClassName={"pagination__link--disabled"}
+        activeClassName={"pagination__link--active"}
+      />
     </div>
   );
 };
