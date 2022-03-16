1979b2ac2c9884bcfbdf1dacd3814f2e2c532d44
kaye garcia <kayemgarcia@gmail.com>
2022-03-16 17:23:08 -0500

    Build profile component to show first and last name. Include edit username functionality

--- client/src/components/User/Profile.jsx
+++ client/src/components/User/Profile.jsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import { useSelector } from "react-redux";
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
 import Card from 'react-bootstrap/Card';
+import LottieUser from "../LottieFiles/LottieUser";
+import { clearUser } from "../../features/userSlice";
+import { useNavigate } from "react-router-dom";
+import ListGroup from 'react-bootstrap/ListGroup';
+import EditProfileForm from "./EditProfileForm";
 
 const Profile = () => {
-  // const shoppingList = useSelector((state) => state.user.shoppingList)
-  // const user = useSelector(state => state.user.firstName)
-  // console.log(user)
+  const username = useSelector(state => state.user.username)
+  const user = useSelector(state => state.user);
+  const firstName = user?.firstName;
+  const lastName = user?.lastName;
+  const id = useSelector(state => state.user.id)
 
-  // const renderShoppingList = () => {
-  //   return shoppingList.map(item => {
-  //     return <li key={item}>
-  //       {item}
-  //     </li>
-  //   })
-  // }
+  const [showForm, setShowForm] = useState(false);
 
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+
+  function deleteProfile() {
+    fetch(`/users/${id}`, {
+      method: "DELETE",
+    })
+    localStorage.removeItem('jwt')
+    dispatch(clearUser())
+    navigate("/")
+  }
+
+  function handleEditClick() {
+    setShowForm((showForm) => !showForm);
+  }
 
   return (
-    <div>
-      {/* {renderShoppingList()} */}
+    <div className="lottie-user">
+      <LottieUser />
+      <Card className="profile-card">
+        <ListGroup>
+          <ListGroup.Item>First Name: {firstName}</ListGroup.Item>
+          <ListGroup.Item>Last Name: {lastName}</ListGroup.Item>
+          <ListGroup.Item>Username: {username}
+            {showForm ? <EditProfileForm /> : null}
+            <div className="edit-username-form">
+              <button className="edit-username-btn" onClick={handleEditClick}><span role="img" aria-label="edit">
+                ✏️
+              </span></button>
+            </div>
+          </ListGroup.Item>
+          <button className="delete-profile-btn" onClick={deleteProfile}>Delete Profile</button>
+        </ListGroup>
+
+
+      </Card>
+
+
     </div>
   );
 };
