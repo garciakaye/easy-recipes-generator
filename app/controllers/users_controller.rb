132684b09c5d3590fa1b7d8e02858e0b68b9233a
kaye garcia <kayemgarcia@gmail.com>
2022-03-17 17:54:51 -0500

    Remove unused before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
-  # before_action :set_user, only: [:show, :update, :destroy]
+  
 
   # GET /users
   def index
@@ -46,12 +46,10 @@ class UsersController < ApplicationController
 	end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:id, :first_name, :last_name, :username, :password, :user_ingredients)
     end
