f6802ce5e1f2e4bac2c402ad3ad7b0e310da6ee9
kaye garcia <kayemgarcia@gmail.com>
2022-03-17 10:31:57 -0500

    Add baseURL for heroku domain

--- client/src/Globals.js
+++ client/src/Globals.js
@@ -1,4 +1,5 @@
-export const baseUrl = "http://localhost:3000";
+// export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://easy-recipes-generator.herokuapp.com/";
 export const headers = {
   "Accept": "application/json",
   "Content-Type": "application/json"
