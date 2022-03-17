fe24a41a1ac99aaf1eb082dbb6212d3f0d397ae1
kaye garcia <kayemgarcia@gmail.com>
2022-03-17 17:54:38 -0500

    Remove unused method

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,8 +2,5 @@ class Ingredient < ApplicationRecord
   has_many :user_ingredients
   has_many :users, through: :user_ingredients
 
-  def self.category_desc
-    all.order(category: :desc, name: :asc)
-  end
 
 end
